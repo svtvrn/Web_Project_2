5a6612b50fcaa5faee98c5774c8acb664fe7ab9e
svtvrn <nikoschasapis99@gmail.com>
2021-05-13 00:54:20 +0300

    template for wid

--- public/scripts/fetchbooks.js
+++ public/scripts/fetchbooks.js
@@ -3,6 +3,8 @@ var searchInput = document.querySelector(".search-bar input[type='text']");
 
 let url = 'https://reststop.randomhouse.com/resources/works/';
 
+const searchResults = document.querySelector('.search-results');
+
 searchBtn.addEventListener('click', fetchBooks);
 
 function fetchBooks(){
@@ -14,6 +16,29 @@ function fetchBooks(){
             }
         })
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => {
+            searchResults.innerHTML = null;
+            if(!data.workid){
+                searchResults.innerHTML = `Couldn't find the book with id: ${searchInput.value}.`;
+            }else{
+                console.log(data);
+                searchResults.innerHTML = resultTemplate(data);
+            }
+        });
     }
+}
+
+function resultTemplate(data){
+
+    return (
+        `<li id="wid-${data.workid}">
+            <p>Title: ${data.titleweb}</p>
+            <p>Author: ${data.authorweb}</p>
+            <p>ID: ${data.workid}</p>
+            <span class="fav-btngroup">
+                <input id="add-fave" type="submit" value="add"></input>
+                <input id="remove-fave" type="submit" value="remove"></input>
+            </span>
+        </li>`
+    );
 }
