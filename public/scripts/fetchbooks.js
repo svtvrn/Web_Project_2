738d825b3f21f6b86edee8d0b0891bb1526a3fc6
svtvrn <nikoschasapis99@gmail.com>
2021-05-14 02:15:45 +0300

    added click function

--- public/scripts/fetchbooks.js
+++ public/scripts/fetchbooks.js
@@ -1,17 +1,17 @@
 var searchBtn = document.querySelector(".search-bar input[type='submit']");
 var searchInput = document.querySelector(".search-bar input[type='text']");
 
-let url = 'https://reststop.randomhouse.com/resources/works/';
+let worksUrl = 'https://reststop.randomhouse.com/resources/works/';
 
 const searchResults = document.querySelector('.search-results');
 
-var books = [];
+var bookData= [];
 
 searchBtn.addEventListener('click', fetchBooks);
 
 function fetchBooks(){
     if(searchInput.value){
-        fetch(url+searchInput.value,{
+        fetch(worksUrl+searchInput.value,{
             method: 'GET',
             headers :{
                 'Accept': 'application/json'
@@ -23,9 +23,7 @@ function fetchBooks(){
             if(!data){
                 searchResults.innerHTML = `Couldn't find the book with id: ${searchInput.value}.`;
             }else{
-                console.log(data);
                 searchResults.innerHTML += resultTemplate(data);
-                createListeners(books);
             }
         })
         .catch(error => console.log(error));
@@ -33,31 +31,26 @@ function fetchBooks(){
 }
 
 function resultTemplate(data){
-    books.push(`wid-${data.workid}`);
+    bookData.push({key: data.workid, value: data});
     return (
         `<li id="wid-${data.workid}">
             <p>Title: ${data.titleweb}</p>
             <p>Author: ${data.authorweb}</p>
             <p>ID: ${data.workid}</p>
             <span class="fav-btngroup">
-                <input id="add-fave" type="submit" value="add"></input>
-                <input id="remove-fave" type="submit" value="remove"></input>
+                <input id="add-fave" type="submit" value="add" onclick="addBook('${data.workid}')"></input>
+                <input id="remove-fave" type="submit" value="remove" onclick="removeBook('${data.workid}')"></input>
             </span>
         </li>`
     );
 }
 
-function createListeners(books){
-    books.forEach(book => {
-        let add = document.querySelector(`#${book} #add-fave`);
-        
-    });
+function addBook(wid){
+    console.log(wid);
+    console.log(bookData[0]);
 }
 
-function addBook(book){
-    console.log(`${book} added`);
-}
-
-function removeBook(book){
-    console.log(`${book} removed`);
+function removeBook(wid){
+    console.log(wid);
+    console.log(bookData);
 }
