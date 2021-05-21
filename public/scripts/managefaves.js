24cd58513b9c5073915b240a0c7423e415cfffcf
svtvrn <nikoschasapis99@gmail.com>
2021-05-21 19:06:57 +0300

    added timeout

--- public/scripts/managefaves.js
+++ public/scripts/managefaves.js
@@ -1,4 +1,10 @@
-var filt = document.querySelector(".filter-bar");
-filt.addEventListener('input', () =>{
-    console.log("woo");
-});
+const filterInput = document.querySelector('.filter-bar');
+
+filterInput.addEventListener('input', filterList);
+
+
+function filterList(){
+    setTimeout(()=>{
+        console.log(filterInput.value);
+    },2000);
+}
